994b336abe7948ccb3b22affe4c53edc8c4077f7
Svet <stsupryk@gmail.com>
2018-11-23 01:12:36 +0200

    first fix

--- script.js
+++ script.js
@@ -5,20 +5,42 @@ var prev = slwrapper.querySelector(".prev");
 var next = slwrapper.querySelector(".next");
 var slide = slider.querySelector(".slider__inner").offsetWidth;
 
+
+
+// prev.addEventListener("click", function() {
+//     sdvig -= slide;
+//     if (sdvig < -slide * 4) {
+//         sdvig = 0;
+//     }
+//     slider.style.left = sdvig + "px";
+// });
+
+// next.addEventListener("click", function() {
+//     sdvig += slide;
+//     if (sdvig > slide * 4) {
+//         sdvig = 0;
+//     }
+//     slider.style.right = sdvig + "px";
+// });
 var sdvig = 0;
+slwrapper.addEventListener("click", function(event) {
 
-prev.addEventListener("click", function() {
-    sdvig -= slide;
-    if (sdvig < -slide * 4) {
-        sdvig = 0;
-    }
-    slider.style.left = sdvig + "px";
-});
+    if (event.target == prev) {
+        sdvig -= slide;
+        console.log(event.target.className);
+        if (sdvig < -slide * 4) {
+            sdvig = 0;
+        }
 
-next.addEventListener("click", function() {
-    sdvig += slide;
-    if (sdvig > slide * 4) {
-        sdvig = 0;
+    } else if (event.target == next) {
+        console.log(event.target.className);
+        sdvig += slide;
+        if (sdvig > slide * 4) {
+            sdvig = 0;
+        }
+
     }
-    slider.style.right = sdvig + "px";
+
+    slider.style.transform = 'translate(' + sdvig + 'px)';
+    console.log(sdvig);
 });
