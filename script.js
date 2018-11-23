d8bdea03e63e9c8f69f8255e6a6499dd42053344
Svet <stsupryk@gmail.com>
2018-11-23 02:19:58 +0200

    finished slider about

--- script.js
+++ script.js
@@ -5,42 +5,18 @@ var prev = slwrapper.querySelector(".prev");
 var next = slwrapper.querySelector(".next");
 var slide = slider.querySelector(".slider__inner").offsetWidth;
 
-
-
-// prev.addEventListener("click", function() {
-//     sdvig -= slide;
-//     if (sdvig < -slide * 4) {
-//         sdvig = 0;
-//     }
-//     slider.style.left = sdvig + "px";
-// });
-
-// next.addEventListener("click", function() {
-//     sdvig += slide;
-//     if (sdvig > slide * 4) {
-//         sdvig = 0;
-//     }
-//     slider.style.right = sdvig + "px";
-// });
 var sdvig = 0;
 slwrapper.addEventListener("click", function(event) {
-
-    if (event.target == prev) {
+    if (event.target == next) {
         sdvig -= slide;
-        console.log(event.target.className);
         if (sdvig < -slide * 4) {
             sdvig = 0;
         }
-
-    } else if (event.target == next) {
-        console.log(event.target.className);
-        sdvig += slide;
-        if (sdvig > slide * 4) {
-            sdvig = 0;
+    } else if (event.target == prev) {
+        if (sdvig != 0) {
+            sdvig += slide;
         }
-
     }
-
     slider.style.transform = 'translate(' + sdvig + 'px)';
     console.log(sdvig);
 });
