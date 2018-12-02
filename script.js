61f2b1fdb1fb9cc54216f2d6b60cc38b7c98c33d
Svet <stsupryk@gmail.com>
2018-12-02 03:06:54 +0200

    add burger menu

--- script.js
+++ script.js
@@ -169,6 +169,28 @@
             }
         }
     }
+    burger();
+    ///burger-menu
+    function burger() {
+        let menu = document.querySelector(".mob-menu");
+        let openbtn = document.querySelector(".js-burger-open");
+        let closebtn = document.querySelector(".js-burger-close");
+
+        openbtn.addEventListener("click", show);
+        closebtn.addEventListener("click", hide);
+
+        function show() {
+            this.classList.remove("burger--visibility");
+            menu.style.transform = ("translate(0)");
+        }
+
+        function hide() {
+            openbtn.classList.add("burger--visibility");
+            menu.style.transform = ("translate(200%)");
+        }
+
+
+    }
 
     ////smoothScroll
 
